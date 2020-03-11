4d18bf557f947bc0880ca6324a0b42e8ba5ff963
Ederson Abreu <ederson.dimm@gmail.com>
2020-03-11 14:15:21 -0300

    removed oauth2 server implementations

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
-// const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-// const Oauth2Server = require('express-oauth-server');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const winston = require('winston');
@@ -16,32 +14,18 @@ const authRoutes = require('./routes/auth');
 const toolRoutes = require('./routes/tool');
 const userRoutes = require('./routes/user');
 const umzugConfig = require('./config/umzug-config');
-// const oauth2Model = require('./oauth2/model');
 const corsFilter = require('./util/cors');
 const apiDocs = require(path.join(__dirname, 'api-docs.json'));
 
 const PORT = process.env.PORT || 3000;
 const expressApp = express();
 const umzug = new Umzug(umzugConfig);
-/*
-const oauth2Server = new Oauth2Server({
-	model: oauth2Model,
-	grants: ['password', 'refresh_token'],
-	accessTokenLifetime: 60 * 60 * 24,
-	// refreshTokenLifeTime: 60 * 60 * 24,
-	allowEmptyState: true,
-  	allowExtendedTokenAttributes: true
-});
-*/
+
 expressApp.use(helmet());
 expressApp.use(corsFilter);
 expressApp.use(cookieParser());
 expressApp.use(bodyParser.json());
 expressApp.use(bodyParser.urlencoded({ extended: true }));
-/*
-expressApp.post('/token', oauth2Server.token());
-expressApp.use(oauth2Server.authenticate());
-*/
 expressApp.use(expressWinston.logger({
 	exitOnError: false,
 	format: winston.format.combine(
